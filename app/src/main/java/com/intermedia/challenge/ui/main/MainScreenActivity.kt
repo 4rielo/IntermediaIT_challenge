2ad9a5f885cab5e88a4477f3c776fb2f5119f9c2
Ariel <ascarafia@gmail.com>
2022-05-06 01:34:45 -0300

    Added start-side drawer menu, with user name and logout button

--- app/src/main/java/com/intermedia/challenge/ui/main/MainScreenActivity.kt
+++ app/src/main/java/com/intermedia/challenge/ui/main/MainScreenActivity.kt
@@ -2,6 +2,7 @@ package com.intermedia.challenge.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
@@ -9,15 +10,23 @@ import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.intermedia.challenge.R
 import com.intermedia.challenge.databinding.ActivityMainScreenBinding
 import com.intermedia.challenge.ui.character_detail.CharacterDetailActivity
+import com.intermedia.challenge.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_main_screen.view.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class MainScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainScreenBinding
     private val mainViewModel: MainViewModel by viewModel()
+
+    val user = Firebase.auth.currentUser
 
     companion object {
         internal const val SELECTED_CHARACTER = "selected_character"
@@ -38,16 +47,23 @@ class MainScreenActivity : AppCompatActivity() {
         }
 
         navController = findNavController(R.id.nav_host_fragment)
-        binding.bottomNavView.apply {
-            setupWithNavController(navController)
-            itemIconTintList = null
-        }
 
+        binding.apply {
+            viewModel = mainViewModel
+            bottomNavView.setupWithNavController(navController)
+            bottomNavView.itemIconTintList = null
+            tvUserName.text = user?.displayName
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
         setObservers()
     }
 
     private fun setObservers(){
         observeGoToCharacterDetail()
+        observeLogout()
     }
 
     private fun observeGoToCharacterDetail(){
@@ -56,6 +72,24 @@ class MainScreenActivity : AppCompatActivity() {
             intent.putExtra(SELECTED_CHARACTER, selectedCharacter)
             startActivity(intent)
         }
+    }
+
+    private fun observeLogout() {
+        mainViewModel.logoutClicked.observe(this) {
+            if(it) {
+                logOut()
+            }
+        }
+    }
+
+    private fun logOut() {
+        AuthUI.getInstance()
+            .signOut(applicationContext)
+            .addOnCompleteListener {
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
     }
 
 }
